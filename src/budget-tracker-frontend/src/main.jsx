4d7ebffedf4ea6ec843bff275e9dc63d4a52b65f
Daniel Ouma <mshindi.daniel@outlook.com>
2025-02-06 18:53:18 +0300

    use bootswatch theme

--- src/budget-tracker-frontend/src/main.jsx
+++ src/budget-tracker-frontend/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.scss';
 
-import "bootstrap/dist/css/bootstrap.min.css";
+import "bootswatch/dist/cerulean/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
